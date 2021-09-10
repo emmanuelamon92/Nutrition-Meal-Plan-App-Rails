baeb5e0ce9bc212b09b225e3b2d6146fb4e122d3
Emmanuel <emmanuelamon92@gmail.com>
2021-09-10 18:28:57 -0400

    updated sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,20 +5,31 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(username: params[:username]) || User.find_by(username: params[:param][:username])
         if user && user.authenticate(params[:password]) || user && user.authenticate(params[:param][:password])
-            session[:user_id] = user.id
-            render json: { user: user }, status: :created
+            login!
+            # session[:user_id] = user.id
+            render json: { loggin_in: true, user: @user }
         else
-            render json: { errors: ["Wrong Username or Password"] }, status: :unauthorized
+            render json: { status: 401, errors: ['Wrong Username or Password'] }
         end
     end
+
+    def is_logged_in?
+        if logged_in? && current_user
+            render json: {logged_in: true, user: current_user}
+        else
+            render json: {logged_in: false, message: ['No Such User']}
+    end
+
 
     # DELETE /logout
     def destroy
         if session[:user_id]
-            session.destroy
-            render json: "Logout Successful"
-        else
-            render json: {errors: ["You are not logged in!"]}, status: :unauthorized
+            logout!
+            render json: {status: 200, logged_out: true}
         end
     end
+
+    def session_params
+        params.require(:user).permit(:username, :password)
+      end
 end
